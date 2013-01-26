b2a6f4658f9d2d84ba04ec910d0fecb592d021c3
VaL <vd@nxc.no>
2013-01-26 16:41:58 +0100

    Added Smtp Transport

--- classes/utils/leieMail.php
+++ classes/utils/leieMail.php
@@ -6,13 +6,14 @@
  * @package leie
  */
 
+
 /**
  * Class to handle sending emails
  */
-class leieMail
+class leieMtaTransport
 {
     /**
-     * Sends email
+     * Sends email using mail()
      *
      * @return (void)
      * @exception leieRunTimeException
@@ -45,6 +46,88 @@ class leieMail
             throw new leieRunTimeException( $e->getMessage() );
         }
     }
+
+}
+
+/**
+ * To send mails using SMTP
+ */
+class leieSmtpTransport
+{
+
+    /**
+     * @var (string)
+     */
+    protected $Host = false;
+
+    /**
+     * @var (string)
+     */
+    protected $Username = false;
+
+    /**
+     * @var (string)
+     */
+    protected $Password = false;
+
+    /**
+     * @var (string)
+     */
+    protected $Port = false;
+
+    /**
+     * @reimp
+     */
+    public function __construct( $host, $username, $password, $port )
+    {
+        $this->Host = $host;
+        $this->Username = $username;
+        $this->Password = $password;
+        $this->Port = $port;
+    }
+
+    /**
+     * Sends email using mail()
+     *
+     * @return (void)
+     * @exception leieRunTimeException
+     */
+    public function send( $emailFrom, $emailToList, $subject, $content )
+    {
+        if ( !is_array( $emailToList ) )
+        {
+            $emailToList = array( $emailToList );
+        }
+
+        try
+        {
+            $mail = new ezcMail();
+            $mail->from = new ezcMailAddress( $emailFrom );
+            foreach ( $emailToList as $email )
+            {
+                $mail->addTo( new ezcMailAddress( $email ) );
+            }
+
+            $mail->subject = $subject;
+            $textPart = new ezcMailText( $content, 'UTF-8' );
+            $textPart->subType = 'html';
+            $mail->body = $textPart;
+
+            $options = new ezcMailSmtpTransportOptions();
+            $options->connectionType = ezcMailSmtpTransport::CONNECTION_SSLV3;
+
+            $transport = new ezcMailSmtpTransport( $this->Host, $this->Username, $this->Password, $this->Port, $options );
+            $transport->options->connectionType = ezcMailSmtpTransport::CONNECTION_SSLV3;
+
+            // Use the SMTP transport to send the created mail object
+            $transport->send( $mail );
+        }
+        catch ( Exception $e )
+        {
+            throw new leieRunTimeException( $e->getMessage() );
+        }
+    }
+
 }
 
 ?>
---
+++ tests/leieMailTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author VaL
+ * @copyright Copyright (C) 2011 VaL::bOK
+ * @license GNU GPL v2
+ * @package leie
+ */
+
+class leieMailTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Checks is set var works
+     */
+    public function testSend()
+    {
+    }
+
+}
+?>
