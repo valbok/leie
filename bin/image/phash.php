4415f24c5bf0f82f1d7057ddbfb2b37fad492cb4
VaL <vd@nxc.no>
2013-11-07 15:09:04 +0100

    imgs

--- bin/image/phash.php
+++ bin/image/phash.php
@@ -12,7 +12,7 @@ $h1 = leieImage::get( $fn1 )->getPerceptualHash();
 print $h1 . "\n";
 if ( $fn2 )
 {
-    $h2 = @leieImage::get( $fn2 )->getPerceptualHash();
+    $h2 = leieImage::get( $fn2 )->getPerceptualHash();
     print $h2 . "\n";
     print $fn1 . " and " . $fn2 + " distance: " + leieImage::getHammingDistance( $h1, $h2 ) . " and " . ( ( ( 64 - leieImage::getHammingDistance( $h1, $h2 ) ) * 100.0 ) / 64.0 ) . "% similar.\n";
 }
