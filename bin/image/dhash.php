49e89581ceb316e8425670c89e72af760c0c3f26
VaL <vd@nxc.no>
2013-11-10 13:27:07 +0100

    - readme

---
+++ bin/image/dhash.php
@@ -0,0 +1,19 @@
+<?php
+require 'autoload.php';
+
+$fn1 = isset( $_SERVER['argv'][1] ) ? $_SERVER['argv'][1] : false;
+if ( !$fn1 )
+{
+    exit;
+}
+
+$fn2 = isset( $_SERVER['argv'][2] ) ? $_SERVER['argv'][2] : false;
+$h1 = leieImage::get( $fn1 )->getDifferenceHash();
+print $h1 . "\n";
+if ( $fn2 )
+{
+    $h2 = leieImage::get( $fn2 )->getDifferenceHash();
+    print $h2 . "\n";
+    print $fn1 . " and " . $fn2 + " distance: " + leieImage::getHammingDistance( $h1, $h2 ) . " and " . ( ( ( 64 - leieImage::getHammingDistance( $h1, $h2 ) ) * 100.0 ) / 64.0 ) . "% similar.\n";
+}
+?>
---
+++ classes/image/README.md
@@ -0,0 +1,5 @@
+leie::image
+-----------
+
+Tools to do some actions on images.
+For example scaling, comparisons, validating etc.
